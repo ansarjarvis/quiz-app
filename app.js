83679e5d3ce84f8ef449840a9c3b2ee6d6655000
ansarjarvis <ansar.ofcl00@gmail.com>
2021-08-09 00:06:52 +0530

    add stylesheet

---
+++ app.js
@@ -0,0 +1,28 @@
+const quizData = [
+    {
+        question: "How Old the Universe is ?",
+        a: " 100 Years",
+        b: " 500 Years",
+        c: " 1000 Years",
+        d: " millions Years",
+        correct: "d"
+    },
+    {
+        question: " Who is the President of Erangle ?",
+        a: " lord M4",
+        b: " Dr. AKM",
+        c: " Mr.M16",
+        d: " None of the above",
+        correct: " a "
+
+    },
+    {
+        question: "HTML stand for ",
+        a: " Hypertext Markup Language",
+        b: " Hydrabad Tamil Mumbai Lost",
+        c: " give me  phone a friend option ",
+        d: " no not possible here",
+        correct: "a"
+
+    }
+]
